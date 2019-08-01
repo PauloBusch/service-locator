831385e742d88c95caf44f380beef8fe316403b3
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-01 20:12:15 -0300

    Validação do teste de novas instâncias.

--- UnitTests/ServiceLocatorTests.cs
+++ UnitTests/ServiceLocatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ServiceLocatorFramework;
 
@@ -7,10 +8,28 @@ namespace UnitTests
     [TestClass]
     public class ServiceLocatorTests
     {
+        private static int TRY_INSTANCES = 10;
+
         [TestMethod]
-        public void Set()
+        public void NewInstances()
         {
-            //IServiceLocator sl = new ServiceLocator();
+            /*
+            IServiceLocator sl = new ServiceLocator();
+            sl.Set<IMock>()
+                .Implements<Mock>()
+                .NewInstancesScope();
+            */
+
+            IList<IMock> InstanceObjects = new List<IMock>();
+
+            for (int i = 0; i < TRY_INSTANCES; i++)
+                InstanceObjects.Add(new Mock()/*sl.Get<IMock>()*/);
+
+            IList<IMock> distinctInstances = InstanceObjects.Distinct().ToList();
+            Assert.AreEqual(TRY_INSTANCES, distinctInstances.Count);
+
+            for (int i = 0; i < TRY_INSTANCES; i++)
+                Assert.AreEqual(i + 1, InstanceObjects[i].Instances);
         }
     }
 }
