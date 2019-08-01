851000d5b669baea10c039a5e5525721afb2854f
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-01 20:49:29 -0300

    Implementação de BindOptions

---
+++ ServiceLocator/BindOptions.cs
@@ -0,0 +1,12 @@
+﻿using ServiceLocatorFramework;
+
+namespace ServiceLocatorFramework
+{
+    public class BindOptions<TInterface> : IBindOptions<TInterface>
+    {
+        public IScopeOptions Implements<TImplements>() where TImplements : TInterface
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
