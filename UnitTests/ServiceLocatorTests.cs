4e8062b19e85051b3b1f5edf9c74329b2201040c
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-01 20:19:02 -0300

    Validando teste de instâncias Singleton

--- UnitTests/ServiceLocatorTests.cs
+++ UnitTests/ServiceLocatorTests.cs
@@ -9,6 +9,12 @@ namespace UnitTests
     public class ServiceLocatorTests
     {
         private static int TRY_INSTANCES = 10;
+        private static int SINGLETON_INSTANCE = 1;
+
+        [TestInitialize]
+        public void Setup() { 
+            Mock.ResetCountInstances();
+        }
 
         [TestMethod]
         public void NewInstances()
@@ -30,6 +36,28 @@ namespace UnitTests
 
             for (int i = 0; i < TRY_INSTANCES; i++)
                 Assert.AreEqual(i + 1, InstanceObjects[i].Instances);
+        }
+        [TestMethod]
+        public void SingletonInstances()
+        {
+            /*
+            IServiceLocator sl = new ServiceLocator();
+            sl.Set<IMock>()
+                .Implements<Mock>()
+                .NewInstancesScope();
+            */
+
+            IList<IMock> InstanceObjects = new List<IMock>();
+
+            var instance = new Mock();
+            for (int i = 0; i < TRY_INSTANCES; i++)
+                InstanceObjects.Add(instance/*sl.Get<IMock>()*/);
+
+            IList<IMock> distinctInstances = InstanceObjects.Distinct().ToList();
+            Assert.AreEqual(SINGLETON_INSTANCE, distinctInstances.Count);
+
+            for (int i = 0; i < TRY_INSTANCES; i++)
+                Assert.AreEqual(SINGLETON_INSTANCE, InstanceObjects[i].Instances);
         }
     }
 }
