a521805f2a8dbfe75371512201921ec948310df7
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-08-01 19:52:39 -0300

    Moks de teste

---
+++ UnitTests/Mock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class Mock : IMock
+    {
+        public int Instances { get => _instances; }
+
+        private static int _instances;
+        public Mock() {
+            _instances++;
+        }
+    }
+}
