a4284e6b7908e46a65ae80a1c908f5eadaab6592
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-02-02 19:23:06 -0300

    Atualização da interface

--- ServiceLocator/IServiceLocator.cs
+++ ServiceLocator/IServiceLocator.cs
@@ -1,7 +1,10 @@
-﻿namespace ServiceLocatorFramework
+﻿using System;
+
+namespace ServiceLocatorFramework
 {
     public interface IServiceLocator
     {
+        object Get(Type type);
         TInterface Get<TInterface>();
         IBindOptions<TInterface> Set<TInterface>();
     }
